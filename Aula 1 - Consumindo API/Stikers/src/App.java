18492aa8bd46db0a423c0ee5bb845f20fc1b97c1
lavianii <jonaslavi02@gmail.com>
2023-03-27 12:12:03 -0300

    separando as classes

--- Aula 1 - Consumindo API/Stikers/src/App.java
+++ Aula 1 - Consumindo API/Stikers/src/App.java
@@ -1,48 +1,30 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
+
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        try {
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        String urlNasa = "https://imersao-java-apis.s3.amazonaws.com/NASA-APOD_1_url.jpg";
 
-            // acessando a url fazendo a conexao HTTP
-            String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
-            URI endereco = URI.create(url);
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
-            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-            String body = response.body();
+        ClienteHttp http = new ClienteHttp();
+        String jsonNasa = http.buscaDados(urlNasa);
 
-            // extrair só os dados que interessam(titulo, imagem, classificacao)
-            var parser = new JsonParser();
-            List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        ExtratorDeConteudo extratorDaNasa = new ExtratorDeConteudoDaNasa();
+        List<Conteudo> conteudosNasa = extratorDaNasa.extraiConteudos(jsonNasa);
 
-            // exibir e manipular os dados
-            for (Map<String, String> filme : listaDeFilmes) {
+        for (Conteudo conteudo : conteudosNasa) {
+            
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.getUrlImagem() + ".png";
 
-                String urlImagem = filme.get("image");
-                String titulo = filme.get("title");
-                
-                InputStream inputStream = new URL(urlImagem).openStream();
-                String nomeArquivo = titulo + ".png";
+            GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
+            geradora.cria(inputStream, nomeArquivo);
 
-                GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
-                geradora.cria(inputStream, nomeArquivo);
-                
-                System.out.println(titulo);
-                System.out.println();
-
-            }
-        } catch (Exception e) {
-        } // Não vai dar erro
+            System.out.println(conteudo.getTitulo());
+        }
 
     }
 }
